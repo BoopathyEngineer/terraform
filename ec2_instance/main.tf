b6a389d452513b9ea0cfb75d5a0aa114ad632a37
S BOOPATHY <95556016+BoopathyEngineer@users.noreply.github.com>
2024-06-19 13:58:00 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,11 +1,15 @@
+#provider
 provider "aws" {
   region = "us-east-1"
 }
-
-
+#help to launch ec2 instance
 resource "aws_instance" "ubuntu" {
-  ami = "ami-00beae93a2d981137"
+  ami="ami-08a0d1e16fc3f61ea"
+  instance_type = "t2.nano"
   key_name = "github"
-  instance_type = "t2.micro"
 }
-
+#optional after while we use instead of terminate make in stop state
+resource "aws_ec2_instance_state" "ubuntu" {
+  instance_id = aws_instance.ubuntu.id
+  state       = "stopped"
+}
